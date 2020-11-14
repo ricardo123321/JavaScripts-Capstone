d3930b21ba00136e0585158e1a4d9c0085c4707b
ricardo <ricardotvera.54@gmail.com>
2020-11-13 21:49:13 -0800

    added more enemies

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -76,16 +76,51 @@ this.playerLasers = this.add.group();
 this.time.addEvent({
   delay: 1000,
   callback: function() {
-    var enemy = new GunShip(
-      this,
-      Phaser.Math.Between(0, this.game.config.width),
-      0
-    );
-    this.enemies.add(enemy);
+    var enemy = null;
+
+    if (Phaser.Math.Between(0, 10) >= 3) {
+      enemy = new GunShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+    else if (Phaser.Math.Between(0, 10) >= 5) {
+      if (this.getEnemiesByType("ChaserShip").length < 5) {
+
+        enemy = new ChaserShip(
+          this,
+          Phaser.Math.Between(0, this.game.config.width),
+          0
+        );
+      }
+    }
+    else {
+      enemy = new CarrierShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+
+    if (enemy !== null) {
+      enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+      this.enemies.add(enemy);
+    }
   },
   callbackScope: this,
   loop: true
 });
+  }
+  getEnemiesByType(type) {
+    var arr = [];
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+      if (enemy.getData("type") == type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
   }
   update(){
     this.player.update();
