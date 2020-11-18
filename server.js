500e5c2ee84c8f17fbc0528eb83ecbc4eee1df15
ricardo <ricardotvera.54@gmail.com>
2020-11-18 13:50:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(`${__dirname}/build`));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
 app.listen(port);
