33350a089e89d5ae8683e44876f1c9fa78494b18
ricardo <ricardotvera.54@gmail.com>
2020-11-18 13:28:05 -0800

    fixed server and index

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8097;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/build`));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
